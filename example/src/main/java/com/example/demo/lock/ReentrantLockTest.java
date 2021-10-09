220aa04d69d0c4438e4ad6cc8a08a54bf1b5a6e9
jiadianxia001 <jiadianxia001@ke.com>
2021-10-09 16:04:00 +0800

    package 重命名

--- example/src/main/java/com/example/demo/lock/ReentrantLockTest.java
+++ example/src/main/java/com/example/demo/lock/ReentrantLockTest.java
@@ -1,5 +1,9 @@
 package com.example.demo.lock;
 
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author Jdx
  * @version 1.0
@@ -7,4 +11,30 @@ package com.example.demo.lock;
  * @date 2021/9/14 15:57
  */
 public class ReentrantLockTest {
+
+    private final Lock lock = new ReentrantLock();
+    private int count;
+
+    public void add(int n) {
+        //获取锁
+        lock.lock();
+        try {
+            count += n;
+        } finally {
+            //释放锁
+            lock.unlock();
+        }
+    }
+}
+
+
+class Counter {
+    private int count;
+
+    public void add(int n) {
+        // synchronized 关键字
+        synchronized (this) {
+            count += n;
+        }
+    }
 }
--- example/src/main/java/com/example/demo/lock/readme.md
+++ example/src/main/java/com/example/demo/lock/readme.md
@@ -1,1 +1,14 @@
 6.锁升级过程，轻量锁可以变成偏向锁么，偏向锁可以变成无锁么，自旋锁，对象头结构，锁状态变化过程
+
+
+### ReentrantLock
+而 ReentrantLock 就是一个普通的类，
+它是基于 AQS(AbstractQueuedSynchronizer)来实现的。
+
+
+#### AQS
+
+
+##### CAS
+
+
